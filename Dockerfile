74482f604f121e6bc50ebd05852afea06ee7edfb
Tira <hong@ithuan.tw>
2023-03-07 14:41:12 +0800

    修掠漏資訊。

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,3 @@ RUN apk update && apk add --no-cache docker-cli aws-cli
 
 COPY scripts/ /app/
 CMD sh /app/start.sh
-#RUN echo "* * * * * echo Fafoy" > /etc/crontabs/root
-#CMD ["crond", "-f", "-d", "8"]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
 >&2 echo 'multiple-databases-backup is starting, backup first.'
 sh /app/backup.sh
 if [ -z "${SCHEDULE}" ];
 then
-  echo "var is blank";
+  >&2 echo "There is not SCHEDULE variable, backup once.";
 else
-  echo "var is set to '${SCHEDULE}'";
+  >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically.";
   sh /app/crontab.sh
 fi
 >&2 echo 'multiple-databases-backup is finished, exiting.'
