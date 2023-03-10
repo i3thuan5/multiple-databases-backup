d00be1131144dc4573e32a3a14621b3e46366cce
Tira <hong@ithuan.tw>
2023-03-10 15:59:18 +0800

    用正規表達驗證格式。

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,8 +6,11 @@ sh /app/backup.sh
 if [ -z "${SCHEDULE:-}" ];
 then
   >&2 echo "There is not SCHEDULE variable, backup once.";
-else
+  >&2 echo 'multiple-databases-backup is finished, exiting.'
+  exit 0
+elif [ "${SCHEDULE:-}" =~ "[^ ] [^ ] [^ ] [^ ] [^ ]" ];
   >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically.";
   sh /app/crontab.sh
+else
+  >&2 echo "The SCHEDULE variable should contain five fields exactly.";
 fi
->&2 echo 'multiple-databases-backup is finished, exiting.'
