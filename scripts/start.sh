eec2ee691bc0125993da2843ad54f23cd47817c2
Tira <hong@ithuan.tw>
2023-03-11 07:40:51 +0800

    查bash ê 正規表示法。
    https://stackoverflow.com/a/28256343

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ if [ -z "${SCHEDULE:-}" ]; then
   >&2 echo "There is not SCHEDULE variable, backup once.";
   >&2 echo 'multiple-databases-backup is finished, exiting.'
   exit 0
-elif [ "${SCHEDULE:-}" =~ "[^ ] [^ ] [^ ] [^ ] [^ ]" ]; then
+elif [[ "${SCHEDULE}" =~ ^([[:punct:]]|[[:alnum:]])*[[:blank:]]([[:punct:]]|[[:alnum:]])*[[:blank:]]([[:punct:]]|[[:alnum:]])*[[:blank:]]([[:punct:]]|[[:alnum:]])*[[:blank:]]([[:punct:]]|[[:alnum:]])*$ ]]; then
   >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically.";
   bash /app/crontab.sh
 else
