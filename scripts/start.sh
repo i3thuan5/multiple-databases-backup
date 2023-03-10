b54108469159f3211882618b544d637bc802ea3a
Tira <hong@ithuan.tw>
2023-03-10 17:11:53 +0800

    攏用bash，才有正規表達式。

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 >&2 echo 'multiple-databases-backup is starting, backup first.'
-sh /app/backup.sh
+bash /app/backup.sh
 if [ -z "${SCHEDULE:-}" ]; then
   >&2 echo "There is not SCHEDULE variable, backup once.";
   >&2 echo 'multiple-databases-backup is finished, exiting.'
   exit 0
 elif [ "${SCHEDULE:-}" =~ "[^ ] [^ ] [^ ] [^ ] [^ ]" ]; then
   >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically.";
-  sh /app/crontab.sh
+  bash /app/crontab.sh
 else
   >&2 echo "The SCHEDULE variable should contain five fields exactly.";
   exit 1
