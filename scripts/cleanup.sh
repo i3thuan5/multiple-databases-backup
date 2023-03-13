65b2c3ba801e86742aa308c48d2ab352e9bc1f81
Tira <hong@ithuan.tw>
2023-03-13 15:20:01 +0800

    加工、月版本。

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,8 +9,15 @@ CONTAINER_NAME=$1
 
 FILE_PATH=$(filepath ${CONTAINER_NAME} "${KEEP_EVERY_BACKUP_IN_HOURS} hours ago")
 
-PRESERVE_FILES=/preserves.list
 ALL_FILES=/all.list
+PRESERVE_FILES=/preserves.list
+
+aws s3api list-objects-v2 \
+    --endpoint-url http://localstack:4566 \
+    --bucket "${S3_BUCKET}" \
+    --prefix "${CONTAINER_NAME}" \
+  | jq .Contents[].Key
+  > "${ALL_FILES}"
 
 aws s3api list-objects-v2 \
     --endpoint-url http://localstack:4566 \
@@ -19,14 +26,34 @@ aws s3api list-objects-v2 \
     --start-after "${FILE_PATH}" \
   | jq .Contents[].Key
   > "${PRESERVE_FILES}"
-aws s3api list-objects-v2 \
-    --endpoint-url http://localstack:4566 \
-    --bucket "${S3_BUCKET}" \
-    --prefix "${CONTAINER_NAME}" \
-  | jq .Contents[].Key
-  > "${ALL_FILES}"
 
-for filename in `cat "${ALL_FILES}" | grep -v --file "${PRESERVE_FILES}"`
+for day in $(seq 1 "${KEEP_DAY_BACKUP_IN_DAYS}")
+do
+  FILE_PATH=$(filepath ${CONTAINER_NAME} "${day} days ago")
+  aws s3api list-objects-v2 \
+      --endpoint-url http://localstack:4566 \
+      --bucket "${S3_BUCKET}" \
+      --prefix "${CONTAINER_NAME}" \
+      --start-after "${FILE_PATH}" \
+      --max-item "1" \
+    | jq .Contents[0].Key
+    >> "${PRESERVE_FILES}"
+done
+
+for month in $(seq 1 "${KEEP_MONTH_BACKUP_IN_MONTHS}")
+do
+  FILE_PATH=$(filepath ${CONTAINER_NAME} "${month} months ago")
+  aws s3api list-objects-v2 \
+      --endpoint-url http://localstack:4566 \
+      --bucket "${S3_BUCKET}" \
+      --prefix "${CONTAINER_NAME}" \
+      --start-after "${FILE_PATH}" \
+      --max-item "1" \
+    | jq .Contents[0].Key
+    >> "${PRESERVE_FILES}"
+done
+
+for filename in `cat "${ALL_FILES}" | grep --invert-match --line-regexp --file "${PRESERVE_FILES}"`
 do
   aws s3api delete-object \
     --endpoint-url http://localstack:4566 \
