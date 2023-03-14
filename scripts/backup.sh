88c9ffc089721b8d41cecdeab6e87538676213ec
Tira <hong@ithuan.tw>
2023-03-14 08:58:58 +0800

    副檔名khioh出來。

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,22 +7,23 @@ export AWS_SECRET_ACCESS_KEY="${S3_SECRET_ACCESS_KEY}"
 
 source /app/filepath.sh
 
+if [ -z "${GPG_PUBLIC_KEY:-}" ]; then
+  ENCRYPT_COMMAND="cat"
+  FILE_TYPE="sql.gz"
+else
+  ENCRYPT_COMMAND="gpg --encrypt --recipient-file ${GPG_PUBLIC_KEY_PATH} -o - -"
+  FILE_TYPE="sql.gz.gpg"
+fi
+
 for postgres_container_name in `docker container list \
     --format "{{.Names}}" \
     --filter label=backup.postgres=true`
 do
     FILE_PATH=$(filepath ${postgres_container_name} 'now')
-    if [ -z "${GPG_PUBLIC_KEY:-}" ]; then
-      ENCRYPT_COMMAND="cat"
-      OUTPUT_FILE_PATH="${FILE_PATH}.sql.gz"
-    else
-      ENCRYPT_COMMAND="gpg --encrypt --recipient-file ${GPG_PUBLIC_KEY_PATH} -o - -"
-      OUTPUT_FILE_PATH="${FILE_PATH}.sql.gz.gpg"
-    fi
     docker exec "${postgres_container_name}" pg_dump -U postgres \
       | gzip \
       | ${ENCRYPT_COMMAND} \
       | aws s3 cp --endpoint-url "${S3_ENDPOINT_URL}" \
-        - "s3://${S3_BUCKET}/${OUTPUT_FILE_PATH}"
-    bash -x /app/cleanup.sh "${postgres_container_name}" "${FILE_PATH}"
+        - "s3://${S3_BUCKET}/${FILE_PATH}.${FILE_TYPE}"
+    bash -x /app/cleanup.sh "${postgres_container_name}"
 done
