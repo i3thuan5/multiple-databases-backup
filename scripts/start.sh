474c3893998169ddfb97e5319552f277c7220495
Tira <hong@ithuan.tw>
2023-03-14 15:57:01 +0800

    文案修hōo kāng-khuán。

--- scripts/start.sh
+++ scripts/start.sh
@@ -34,17 +34,18 @@ else
   echo ${GPG_PUBLIC_KEY} | base64 -d  > ${GPG_PUBLIC_KEY_PATH}
 fi
 if [ -z "${SCHEDULE}" ]; then
-  >&2 echo "multiple-databases-backup is starting. There is not SCHEDULE variable, backup once.";
+  >&2 echo "multiple-databases-backup is starting."
+  >&2 echo "There is not SCHEDULE variable, backup once."
   bash /app/backup.sh
   >&2 echo 'multiple-databases-backup is finished, exiting.'
   exit 0
 elif [[ "${SCHEDULE}" =~ ^([^[:blank:]]*[[:blank:]]){4}([^[:blank:]])*$ ]]; then
   >&2 echo 'multiple-databases-backup is starting.'
-  >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically.";
-  >&2 echo "And backup right now at the same time.";
+  >&2 echo "There is the SCHEDULE variable: '${SCHEDULE}', backup periodically."
+  >&2 echo "And backup right now at the same time."
   bash /app/backup.sh &
   bash /app/crontab.sh
 else
-  >&2 echo "The SCHEDULE variable should contain five fields exactly.";
+  >&2 echo "The SCHEDULE variable should contain five fields exactly."
   exit 1
 fi
