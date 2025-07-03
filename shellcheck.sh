ec192c55517b4253c5297f6c04eacdfffbabde50
Tia i X570-ACE <hong@ithuan.tw>
2025-07-03 16:39:00 +0800

    修SC1091

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 exit_code=0
+shellcheck --severity=info scripts/*;
+tsitkai="$?"
+exit_code=$(( tsitkai != 0 ? tsitkai : exit_code))
 while IFS= read -r -d '' file
 do
     shellcheck --severity=info "$file";
     tsitkai="$?"
     exit_code=$(( tsitkai != 0 ? tsitkai : exit_code))
-done <   <(find . -type f -name '*.sh' -not -path './venv/*' -print0)
+done <   <(find . -type f -name '*.sh' -not -path './venv/*' -not -path './scripts/*' -print0)
 exit $(( exit_code == 0 ? 0 : 1))
