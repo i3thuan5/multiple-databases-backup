69353b4e78c027cf53821c52332464c0f37d9b7e
Tia i X570-ACE <hong@ithuan.tw>
2025-07-04 11:41:50 +0800

    修正cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -58,8 +58,8 @@ sort -u "${ALL_FILES}" > "${temp_dir}/tmp.list"
 mv "${temp_dir}/tmp.list" "${ALL_FILES}"
 sort -u "${PRESERVE_FILES}" > "${temp_dir}/tmp.list"
 mv "${temp_dir}/tmp.list" "${PRESERVE_FILES}"
-comm -1 "${ALL_FILES}" "${PRESERVE_FILES}" \
-  | sed 's/^[[:space:]]"\(.*\)"$/\1/g' \
+comm -23 "${ALL_FILES}" "${PRESERVE_FILES}" \
+  | sed 's/^[[:space:]]*"\(.*\)"$/\1/g' \
   | while IFS= read -r filename
 do
   aws s3api delete-object \
